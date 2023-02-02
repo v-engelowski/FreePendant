9fe748d6c1b12af4d68f4b10c6f6368dd4dd5d0d
Maker-Macher <63427357+Maker-Macher@users.noreply.github.com>
2023-02-02 16:16:12 +0100

    Add project files.

---
+++ FreePendant/FreePendant.cs
@@ -0,0 +1,33 @@
+﻿using BepInEx;
+using UnityEngine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HarmonyLib;
+
+namespace FreePendant
+{
+    [BepInPlugin("makermacher.freependant", "FreePendant", "1.0")]
+    public class FreePendant : BaseUnityPlugin
+    {
+        void Awake()
+        {
+            new Harmony("makermacher.freependant").PatchAll();
+
+            Logger.LogMessage("Loaded and patched");
+        }
+    }
+
+    [HarmonyPatch(typeof(HeroMerchant), "GetPendantCost")]
+    public class Patch
+    {
+        [HarmonyPrefix]
+        public static bool FreePendant(ref int __result)
+        {
+            __result = 0;
+            return false;
+        }
+    }
+}
